c8f2ac444832c1c70db3e369702854b5a37e5e7e
lva100 <lva100@ya.ru>
2022-05-17 08:07:18 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.scss'
+import { Car } from "./src/car"
+
+const canvas = document.getElementById("myCanvas");
+canvas.height = window.innerHeight;
+canvas.width = 300;
+
+const ctx = canvas.getContext("2d");
+const car = new Car(100,100,30,50);
+car.draw(ctx);
+
