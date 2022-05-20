4426f82cdead82c36e92cbb49ddb7652208f22c0
lva100 <lva100@ya.ru>
2022-05-20 15:43:04 +0300

    Made simulating traffic

--- src/car.js
+++ src/car.js
@@ -4,7 +4,7 @@ import { polysIntersect } from "./utils";
 
  
 export class Car {
-    constructor(x,y,width,height) {
+    constructor(x,y,width,height,controlType,maxSpeed=3) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -12,25 +12,36 @@ export class Car {
 
         this.speed = 0;
         this.acceleration = 0.2;
-        this.maxSpeed = 3;
+        this.maxSpeed = maxSpeed;
         this.friction = 0.05;
         this.angle = 0;
-        this.damaged = true;
+        this.damaged = false;
         
-        this.sensor = new Sensor(this);
-        this.controls = new Controls();
+        if(controlType!="DUMMY") {
+            this.sensor = new Sensor(this);
+        }
+        this.controls = new Controls(controlType);
     }
 
-    update(roadBorders) {
-        this.#move();
-        this.polygon = this.#createPolygon();
-        this.damaged = this.#assessDamage(roadBorders);
-        this.sensor.update(roadBorders);
+    update(roadBorders,traffic) {
+        if (!this.damaged) {
+            this.#move();
+            this.polygon = this.#createPolygon();
+            this.damaged = this.#assessDamage(roadBorders,traffic);
+        }
+        if(this.sensor) {
+            this.sensor.update(roadBorders,traffic);
+        }
     }
 
-    #assessDamage(roadBorders) {
+    #assessDamage(roadBorders,traffic) {
         for(let i=0; i<roadBorders.length; i++) {
             if(polysIntersect(this.polygon, roadBorders[i])) {
+                return true;
+            }
+        }
+        for(let i=0; i<traffic.length; i++) {
+            if(polysIntersect(this.polygon, traffic[i].polygon)) {
                 return true;
             }
         }
@@ -97,11 +108,11 @@ export class Car {
         this.y -= Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx) {
+    draw(ctx,color) {
         if(this.damaged) {
-            ctx.fillStyle = "red";
+            ctx.fillStyle = "gray";
         } else {
-            ctx.fillStyle = "black";
+            ctx.fillStyle = color;
         }
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
@@ -110,6 +121,8 @@ export class Car {
         }
         ctx.fill();
 
-        this.sensor.draw(ctx);
+        if(this.sensor) {
+            this.sensor.draw(ctx);
+        }    
     }
 }
